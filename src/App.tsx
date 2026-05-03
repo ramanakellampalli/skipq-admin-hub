4f14640652898722062f2807a7f9a4701a8d0f0c
Ramana <ramanakellampalli@gmail.com>
2026-05-02 21:34:55 -0500

    feat: add /home route alias for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/vendors" element={<ProtectedRoute><Vendors /></ProtectedRoute>} />
           <Route path="/campuses" element={<ProtectedRoute><Campuses /></ProtectedRoute>} />
